b7c55bcfa318b81f077086c79d4427cd9f1b31ac
sothatc <dkdltbem37@gmail.com>
2023-11-08 21:39:15 +0900

    [study] api middleware study

--- src/pages/[username]/[info].js
+++ src/pages/[username]/[info].js
@@ -1,17 +1,37 @@
 import Layout from 'components/Layout';
 import SubLayout from 'components/SubLayout';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function userNameInfo() {
   const [time, setTime] = useState();
+  const [name, setName] = useState('?');
 
   const router = useRouter();
-  const {username, info} = router.query;
+  const {username, info, uid} = router.query;
+
+  // useEffect(() => {
+  //   fetch('/api/user')
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setName(data.name);
+  //     })
+  // },[])
+  useEffect(() => {
+    if(uid !== null) {
+      fetch(`/api/user-info/${uid}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setName(data.name);
+        });
+    }
+  },[uid])
+
 
   return (
     <>
       <h1>{username}'s {info}</h1>
+      <h1>Name: {name}</h1>
     </>
   );
 }
